40ee9148c9176a9d3bf9e73197b8b47404d4f10e
LIan2006-pet <liana_petrosyan@edu.aua.am>
2025-05-09 21:11:56 +0400

    Delete NoEnergyException.java

--- NoEnergyException.java
+++
@@ -1,8 +0,0 @@
-public class NoEnergyException extends Exception {
-    public NoEnergyException() {
-        super("No energy");
-    }
-    public NoEnergyException(String message) {
-        super(message);
-    }
-}
