6c314eac78bb93911f3339dfe5548f46274fed6e
88888888888-im <izabella.matinyan1@gmail.com>
2025-05-10 16:39:56 +0400

    Add files via upload

---
+++ exceptions/NotHappyException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class NotHappyException extends Exception {
+    public NotHappyException() {
+        super();
+    }
+    public NotHappyException(String message) {
+        super(message);
+    }
+}
---
+++ exceptions/isHungryException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class isHungryException extends Exception{
+
+    public isHungryException(){
+        super();
+    }
+
+    public isHungryException(String message){
+        super(message);
+    }
+}
